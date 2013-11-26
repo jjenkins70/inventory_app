cbbcc7673445ec1d21effa867e073d1f7f20f650
Jeremiah Jenkins <jjenkins70@gmail.com>
2013-11-26 10:17:26 -0500

    baseline rails configuration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InventoryApp::Application.config.secret_key_base = '9d5d3048b0a2c9936a5342da5320330efa78743c05d30c0ca6fb5f2a00f74cd8644e883fe2dbd9a46e209109d49ad9c14973a619be1b5c29ae2b1ea70db1b730'
