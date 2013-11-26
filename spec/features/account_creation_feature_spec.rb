2e2e5900dcb389c6b29e75f9d540cb583e01337c
Jeremiah Jenkins <jjenkins70@gmail.com>
2013-11-26 10:57:25 -0500

    working on initial test cases

---
+++ spec/features/account_creation_feature_spec.rb
@@ -0,0 +1,19 @@
+#Describe what a user goes through when creating an account
+#
+require 'spec_helper'
+
+describe 'account creation' do
+	if 'allows user to create account' do
+		visit root_path
+		click_link 'Create Account'
+
+		fill_in 'First Name', with: 'Jeremiah'
+		fill_in 'Last Name', with: 'Jenkins'
+		fill_in 'Email', with: 'jjenkins70@gmail.com'
+		fill_in 'Password', with: 'password'
+		fill_in 'Password Confirmation', with: 'password'
+		click_button 'Create Account'
+
+		expect(page).to have_content('Signed up successfully')
+	end
+end
